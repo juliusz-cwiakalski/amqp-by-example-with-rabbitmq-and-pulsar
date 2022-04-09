8a90f104f7b1b965736e7e64fb8e6e32bfe35a36
Juliusz Ćwiąkalski <juliusz@cwiakalski.com>
2022-04-09 07:33:37 +0200

    Projects generated with micronaut CLI

---
+++ publisher/src/test/java/pl/jcw/demo/amqp/PublisherTest.java
@@ -0,0 +1,21 @@
+package pl.jcw.demo.amqp;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class PublisherTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
